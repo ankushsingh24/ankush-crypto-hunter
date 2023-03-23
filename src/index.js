ae9098d1ec474c42fee1d7a5e778db4deec0ca3c
Ankush Singh <ankushsahil45@gmail.com>
2023-03-23 23:12:32 +0530

    added cryptocontext

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "react-alice-carousel/lib/alice-carousel.css";
 import App from "./App";
+import CryptoContext from "./CryptoContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CryptoContext>
+      <App />
+    </CryptoContext>
   </React.StrictMode>
 );
