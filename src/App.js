b304a591df0dd33e8b13de45395a01400db1cafd
Ankush Singh <ankushsahil45@gmail.com>
2023-08-01 20:37:45 +0530

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" component={Homepage} exact />
+          <Route path="/coins/:id" component={CoinPage} exact />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
